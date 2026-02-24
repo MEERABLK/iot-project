6d89a9fe7abb9057176ef781ea0d00da152e7326
JonathanMarkovic <jonathan.markovic@outlook.com>
2026-02-23 21:02:39 -0500

    Created functions to display either a success or a failure
    Added function to light up a success LED
    
    Added function to light up error LED and activate buzzer sound
    
    Added a .gitignore file

---
+++ result.py
@@ -0,0 +1,34 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+successLed = 17
+failLed = 16
+buzzer = 21
+
+def success():
+    GPIO.setup(successLed, GPIO.OUT)
+    GPIO.output(successLed, 1)
+    time.sleep(1)
+    GPIO.output(successLed, 0)
+    
+
+def failure():
+    GPIO.setup(failLed, GPIO.OUT)
+    GPIO.setup(buzzer, GPIO.OUT)
+
+    GPIO.output(failLed, 1)
+    GPIO.output(buzzer, True)
+    print("Failure detected!")
+    print("Buzzer On")
+    time.sleep(1)
+    GPIO.output(failLed, 0)
+    GPIO.output(buzzer, False)
+    print("Buzzer Off")
+
+
+success()
+time.sleep(2)
+failure()
+GPIO.cleanup()
