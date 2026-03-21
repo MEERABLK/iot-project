b263a9568a9db07c97f18f15002d811db8fd3fad
JonathanMarkovic <jonathan.markovic@outlook.com>
2026-03-21 15:32:14 -0400

    Made changes to allow scanning for 2 different temperatures/humidities
    Added a script to sense the data from two different dht11s
    
    added testing.py for testing the new scripts

---
+++ scripts/sensor_data.py
@@ -0,0 +1,42 @@
+import paho.mqtt.client as mqtt
+
+BROKER = "172.20.10.4"
+PORT = 1883
+
+class SensorData:
+    def __init__(self):
+        self.fridge1Temperature = None
+        self.fridge1Humidity = None
+        self.fridge2Temperature = None
+        self.fridge2Humidity = None
+
+data = SensorData()
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code", rc)
+    client.subscribe("#")  # subscribe to all topics
+
+def on_message(client, userdata, msg):
+    payload = msg.payload.decode()
+
+    if msg.topic == "fridge1/temperature":
+        data.fridge1Temperature = float(payload)
+
+    elif msg.topic == "fridge1/humidity":
+        data.fridge1Humidity = float(payload)
+
+    elif msg.topic == "fridge2/temperature":
+        data.fridge2Temperature = float(payload)
+
+    elif msg.topic == "fridge2/humidity":
+        data.fridge2Humidity = float(payload)
+
+    print(f"{msg.topic} -> {payload}")
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+def start():
+    client.connect(BROKER, PORT, 60)
+    client.loop_start()
